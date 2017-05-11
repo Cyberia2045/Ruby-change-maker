f719aa6e411412c27bb008f0057841d1ed3eee69
Cyberia 2045 <sagansociety86@gmail.com>
2017-05-11 10:53:52 -0400

    refactored the even number solution with the built-in Ruby methods

--- even.rb
+++ even.rb
@@ -1,11 +1,18 @@
 # Write a method that takes an array of numbers and creates a new array with only the even numbers from the first array.
 
+	# def even(array)
+	# 	new_array = []
+	# 	array.each do |number|
+	# 		if number % 2 == 0
+	# 			new_array.push(number)
+	# 		end
+	# 	end
+	# 			p new_array
+	# end
+
 	def even(array)
-		new_array = []
-		array.each do |number|
-			if number % 2 == 0
-				new_array.push(number)
-			end
+		array.select! do |number|
+			number.even?
 		end
-				p new_array
+		array
 	end
